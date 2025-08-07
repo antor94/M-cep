d4e0c70cce784e24960db9eb8392aba5903ecf6b
Antor Web-Dev <antorremeni@gmail.com>
2025-08-07 16:34:46 +0600

    change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,14 +10,12 @@ const Navbar = () => {
     
     
     
-    <section   style={{ background: `URL(${banner})`, backgroundRepeat: `no-repeat`, backgroundSize: `cover`, }} id='navbar' className='pt-[29px]'>
-        <div className="container">
+    <nav className="flex justify-between items-center px-8 py-4 bg-transparent text-white absolute w-full z-10">
+        <div className='container'>
             <div id='navbar_row' className='flex  justify-between items-center'>
 
-                {/* -------------------- nav-logo */}
                 <Link className='w-[96px]'><img src={logo} alt="nav-logo" /></Link>
 
-                {/* ----------------  nav-item */}
                 <div>
                     <ul className='flex items-center gap-[50px]'>
                         <li><Link className='text-[16px] font-normal font-inter text-[#fff]'>About</Link></li>
@@ -29,11 +27,40 @@ const Navbar = () => {
                     </ul>
                 </div>
 
-                {/* ------------------- nav-btn */}
+         
+                <button ><HiMiniBars3 className='text-[#fff] text-[32px]' /></button>
+            </div>
+
+
+        </div>
+    </nav>
+
+
+
+
+
+    {/* <section   style={{ background: `URL(${banner})`, backgroundRepeat: `no-repeat`, backgroundSize: `cover`, }} id='navbar' className='pt-[29px]'>
+        <div className="container">
+            <div id='navbar_row' className='flex  justify-between items-center'>
+
+                <Link className='w-[96px]'><img src={logo} alt="nav-logo" /></Link>
+
+                <div>
+                    <ul className='flex items-center gap-[50px]'>
+                        <li><Link className='text-[16px] font-normal font-inter text-[#fff]'>About</Link></li>
+                        <li><Link className='text-[16px] font-normal font-inter text-[#fff]'>Project</Link></li>
+                        <li><Link className='text-[16px] font-normal font-inter text-[#fff]'>Blog</Link></li>
+                        <li><Link className='text-[16px] font-normal font-inter text-[#fff]'>Pricing</Link></li>
+                        <li><Link className='text-[16px] font-normal font-inter text-[#fff]'>Studio</Link></li>
+                        <li><Link className='text-[16px] font-normal font-inter text-[#fff]'>Contact</Link></li>
+                    </ul>
+                </div>
+
+         
                 <button ><HiMiniBars3 className='text-[#fff] text-[32px]' /></button>
             </div>
         </div>
-    </section>
+    </section> */}
     
     
     
